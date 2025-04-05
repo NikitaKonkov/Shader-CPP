f784a266bb471b014199452fe61824ad247a942b
NikitaKonkov <nikita.wagner97@gmail.com>
2025-04-05 11:37:41 +0100

    small fix for displaying legacy shader

--- build-legacy/src/main.cpp
+++ build-legacy/src/main.cpp
@@ -65,15 +65,15 @@ int main(int argc, char* argv[]) {
     // Set up vertex data (a quad made of two triangles)
     float vertices[] = {
         // positions        // texture coords
-        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, // top left
-         0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f  // bottom left
+        -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,  // top left
+         1.0f,  1.0f, 0.0f, 1.0f, 1.0f,  // top right
+         1.0f, -1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f   // bottom left
     };
     
     unsigned int indices[] = {
-        0, 1, 2, // first triangle
-        0, 2, 3  // second triangle
+        0, 1, 2,  // first triangle
+        0, 2, 3   // second triangle
     };
     
     // Create VAO, VBO, and EBO
